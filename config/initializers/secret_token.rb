4e732e7ecff4e916aef21bbbdecc4829de86cf68
dreamdrew <drew@drew-macbook.local>
2013-09-23 04:00:00 -0400

    rerunner project created
    rails new rerunner (baseline)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rerunner::Application.config.secret_key_base = 'd83c6240e22c5773fee4907fc252959c7fcef1bd488e07d77fa13722d78f8d8c9de178f0ccaaba0c9caba5a00a08e47ccd3f7a78d3f7d6a7ebf6bb11ad3b5be9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rerunner::Application.config.session_store :cookie_store, key: '_rerunner_session'
